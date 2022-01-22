ae90ddd582409506b18267f9e5b1294fe2af720f
Alexandru Marian <82831485+Alex61-byte@users.noreply.github.com>
2022-01-22 17:34:55 +0200

    Update Maps.js

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -10,19 +10,19 @@ export default function Maps({ usersPlace, src }) {
     if (usersPlace !== "" && src === "") {
         return (
             <div className="map-container">
-                <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBUzYDHUwbOcWeXObJ4KYRZMAQLZG3zpVU&q=${usersPlace}`} frameBorder="0" title="gmap" width="100%" height="200" loading="lazy" allowFullScreen></iframe>
+                <iframe src={`https://www.google.com/maps/embed/v1/place?key=yourAPIKEY&q=${usersPlace}`} frameBorder="0" title="gmap" width="100%" height="200" loading="lazy" allowFullScreen></iframe>
             </div>
         )
     } else if (usersPlace !== "" && (src !== "" && src !== null)) {
         return <div className="map-container">
             <div className="post-hidden-map">
-                <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBUzYDHUwbOcWeXObJ4KYRZMAQLZG3zpVU&q=${usersPlace}`} frameBorder="0" title="gmap" width="100%" height="200" loading="lazy" allowFullScreen></iframe>
+                <iframe src={`https://www.google.com/maps/embed/v1/place?key=yourAPIKEY&q=${usersPlace}`} frameBorder="0" title="gmap" width="100%" height="200" loading="lazy" allowFullScreen></iframe>
             </div>
         </div>
 
     } else if (usersPlace !== "" && src === null) {
         return <div className="map-container">
-            <iframe src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBUzYDHUwbOcWeXObJ4KYRZMAQLZG3zpVU&q=${usersPlace}`} frameBorder="0" title="gmap" width="100%" height="200" loading="lazy" allowFullScreen></iframe>
+            <iframe src={`https://www.google.com/maps/embed/v1/place?key=yourAPIKEY&q=${usersPlace}`} frameBorder="0" title="gmap" width="100%" height="200" loading="lazy" allowFullScreen></iframe>
         </div>
     } else {
         return null
